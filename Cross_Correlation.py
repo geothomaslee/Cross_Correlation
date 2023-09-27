653bb1d57dc486c6355d76cb275b93d596fc5060
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-09-27 09:32:06 -0600

    Update Cross_Correlation.py

--- Cross_Correlation.py
+++ Cross_Correlation.py
@@ -27,19 +27,58 @@ ax[0].set_ylim((-1 * margin), margin)
 ax[1].plot(time_series, func_2)
 ax[1].set_ylim((-1 * margin), margin)
 
-
+def correlation(a, b):
+    """
+    a: numpy vector of length n
+    b: numpy vector of length n
+    
+    Returns: un-normalized correlation values
+    ---------
+    See Basic_Correlation.py for notes on how this function works
+    """
+    correlation = 0 # Begin with a value of 0
+    
+    for index in range(len(a)):
+        add_correlation = a[index] * b[index]
+        correlation += add_correlation
+        
+    return correlation
 
 def cross_correlation(func_1, func_2, time):
+    """
+    a: numpy vector of length n
+    b: numpy vector of length n
+    
+    Returns: un-normalized correlation values
+    ---------
+    See Basic_Correlation.py for notes on how this function works
+    """
     zeros_vector = np.zeros((len(time),))
     
-    zeroed_func_1 = np.concatenate((zeros_vector, func_1, zeros_vector))
+    # Centers the time_series in a new matrix with zeros on either end
+    # to a distance equal to the length of func_2
     
-    return zeroed_func_1
+    func_1_full = np.concatenate((zeros_vector, func_1, zeros_vector))
+    
+    lag_index = np.arange(0, len(func_1_full), 1)
+    
+    for lag in lag_index:
+        # Figuring out how much empty space we have to the left and right
+        # of func_2 as we sweep it from left to right
+        lag_left = np.zeroes((lag,))
+        lag_right = np.zeroes((len(func_1_full) - lag - len(func_1),))
+        
+        func_2_positioned = np.concatenate((lag_left, func_2, lag_right))
+        
+    
+        
+        
+    
+    
+
 
 test = cross_correlation(func_1, func_2, time_series)
-print(test)
-print(type(test))
-print(test.shape)
+
     
     
         
