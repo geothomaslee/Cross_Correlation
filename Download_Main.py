9dd861810078fe7c79441bdb5b2a4c617b4f7f1d
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-10-24 17:35:39 -0600

    Update Download_Main.py

--- Download_Main.py
+++ Download_Main.py
@@ -10,17 +10,20 @@ from ambient_download.cut_data import cut_traces_into_windows
 from ambient_download.save_streamv2 import save_stream
 from obspy import UTCDateTime
 import obspy
+import time
 
 
 def main():
     
-    days = 3
+    days = 10
     start_day = UTCDateTime("2023-06-06T00:00:00.000")
     
     stream = obspy.core.stream.Stream()
     
+    download_start_time = time.perf_counter()
+    
     for day in range(days):
-        int_start_time = start_day + (15*86400)
+        int_start_time = start_day + (day*86400)
         current_stream = download_trace(client="IRIS",
                                         network="IU",
                                         station="ANMO",
@@ -30,7 +33,9 @@ def main():
                                         timewindow = 86400)
         stream.append(current_stream[0])
     
-    print('Successfully downloaded data')
+    print(f'Successfully downloaded data in {time.perf_counter - download_start_time} seconds')
+    
+    cut_start_time = time.perf_counter()
     
     for i, trace in enumerate(stream):
         print(i)
@@ -41,6 +46,7 @@ def main():
         save_stream(stream=cut_stream,sort_method=sort_method_list,adding_data=True)
         print('Successfully saved traces')
     
+    print(f'Successfully cut and saved data with an average time of {(time.perf_counter() - cut_start_time) / days} seconds per day')
 if __name__ == '__main__':
     main()
     
