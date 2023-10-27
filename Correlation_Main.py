c4bfa77268821423e4dea8e5cadcc5cde9574880
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-10-26 23:03:28 -0600

    Update main

--- Correlation_Main.py
+++ Correlation_Main.py
@@ -7,13 +7,21 @@ Created on Wed Oct  4 08:53:27 2023
 
 from correlation import prep_files as prep
 from correlation import cross_correlate_ambient_noise as corr
-
+import matplotlib.pyplot as plt
 
 station1_files, station2_files = prep.find_station_files('ANMO', 'TUC', '~/Documents/Correlation_Testing_Data')
 
 df1, df2 = prep.get_info_from_file_name(station1_files, station2_files, name_structure=None)
 
 pairs_list = prep.create_corresponding_files_list(df1, df2)
+
+
+test_pair = [pairs_list[0][1],pairs_list[0][1]]
+xcorr_times, xcorr = corr.cross_correlate_ambient_noise(test_pair,low=0.05)
+
+plt.plot(xcorr_times, xcorr)
+
+
 
 
 
--- correlation/cross_correlate_ambient_noise.py
+++ correlation/cross_correlate_ambient_noise.py
@@ -11,21 +11,25 @@ from obspy.signal.cross_correlation import correlate
 from obspy.signal.filter import bandpass
 import matplotlib.pyplot as plt
 
-def create_ambient_times(npts,delta):
-    seconds = npts * delta
-    times = np.arange((0 - seconds), seconds+delta)
+def create_ambient_times(npts,delta,method='points'):
+    if method == 'points':
+        times = np.arange((0-npts),npts+1,1)
+    elif method == 'seconds':
+        seconds = npts * delta
+        times = np.arange((0 - seconds), seconds+delta, delta)
+        
     return times
 
-def cross_correlate_ambient_noise(pair,low,high=None):
+def cross_correlate_ambient_noise(pair,low,high=None,time_method='points'):
     trace1 = obspy.read(pair[0])[0]
     trace2 = obspy.read(pair[1])[0]
     
     npts=trace1.stats['npts']
-    delta=trace2.stats['delta']
+    delta=trace1.stats['delta']
     sampling_rate = 1/delta
     
     if high == None:
-        high_freq = sampling_rate / 2
+        high_freq = (sampling_rate / 2) - delta
     elif type(high) == int or type(high) == float:
         high_freq = high
     else:
@@ -35,14 +39,13 @@ def cross_correlate_ambient_noise(pair,low,high=None):
     trace1_filtered = bandpass(trace1,freqmin=low,freqmax=high_freq,
                                df=sampling_rate,corners=4)
     
-    trace2_filtered = trace1.detrend()
+    trace2_filtered = trace2.detrend()
     trace2_filtered = bandpass(trace2,freqmin=low,freqmax=high_freq,
                                df=sampling_rate,corners=4)
     
-    xcorr = correlate(trace1_filtered, trace2_filtered,normalize=None)
+    xcorr = correlate(trace1_filtered, trace2_filtered,normalize=None,shift=npts)
     
-    xcorr_times = create_ambient_times(npts,delta)
-    
+    xcorr_times = create_ambient_times(npts,delta,time_method)
     return xcorr_times, xcorr
    
 """ 
