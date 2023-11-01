7c01b19196c826150e00f1e9455b937562dd8c8d
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-01 12:18:42 -0600

    Update main
    - Improve documentation of cross_correlate_ambient_noise
    - Functioning example in main

--- Correlation_Main.py
+++ Correlation_Main.py
@@ -17,45 +17,11 @@ df1, df2 = prep.get_info_from_file_name(station1_files, station2_files, name_str
 
 pairs_list = prep.create_corresponding_files_list(df1, df2)
 
-#test_pair = [pairs_list[0][0],pairs_list[0][1]]
-#xcorr_times, xcorr, xcorr_meta = corr.cross_correlate_ambient_noise(test_pair,low=0.05)
+xcorr_list, xcorr_times = corr.multi_correlate(pairs_list,low=0.05)
 
-x_corr_list = []
+xcorr_stack = corr.xcorr_stack(xcorr_list)
 
-for pair in pairs_list:
-    xcorr_times, xcorr, xcorr_meta = corr.cross_correlate_ambient_noise(pair, low=0.05)
-    x_corr_list.append(xcorr)
-    
-len_list = []
-for i, xcorr in enumerate(x_corr_list):
-    len_list.append(len(xcorr))
-    
-npts_expected = mode(len_list)
-print(f'Mode: {npts_expected}')
-
-for i, xcorr in enumerate(x_corr_list):
-    print(len(xcorr))
-    if len(xcorr) == npts_expected:
-        pass
-    else:
-        xcorr_new = xcorr.copy()
-        add_pts = npts_expected - len(xcorr)
-        print(f'Adding {add_pts} points!')
-        for add in range(add_pts):
-            xcorr_new = np.append(xcorr_new, 0)
-        
-        print(xcorr_new[-1])
-            
-        x_corr_list[i] = xcorr_new
- 
-len_list = []
-for i, xcorr in enumerate(x_corr_list):
-    len_list.append(len(xcorr))
-    
-
-x_corr_stack = sum(x_corr_list) / len(x_corr_list)
-
-plt.plot(xcorr_times,x_corr_stack)
+plt.plot(xcorr_times,xcorr_stack)
     
     
 
--- correlation/cross_correlate_ambient_noise.py
+++ correlation/cross_correlate_ambient_noise.py
@@ -10,6 +10,7 @@ import numpy as np
 from obspy.signal.cross_correlation import correlate
 from obspy.signal.filter import bandpass
 import matplotlib.pyplot as plt
+from statistics import mode
 
 def create_ambient_times(npts,delta,method='points'):
     if method == 'points':
@@ -30,7 +31,7 @@ def cross_correlate_ambient_noise(pair,low,high=None,time_method='points'):
     if npts1 == npts2:
         npts = npts1
     else:
-        print(f'Files have different number of points. Using npts1')
+        print(f'Files have different number of points. Using npts1 {npts1}')
         npts = npts1
     
     delta=trace1.stats['delta']
@@ -64,25 +65,71 @@ def cross_correlate_ambient_noise(pair,low,high=None,time_method='points'):
     meta['julday'] = trace1.stats['starttime'].julday
     meta['hour'] = trace1.stats['starttime']
 
-    return xcorr_times, xcorr, meta
+    return xcorr, xcorr_times, meta
+
+def check_correlation_length(xcorr_list):
+    """
+    Parameters
+    ----------
+    xcorr_list : list of numpy.ndarray
+        List of cross-correlation functions given as numpy arrays
+    Returns
+    -------
+    xcorr_list : numpy.ndarray
+        Numpy array containing a single stacked correlation function.
+    """
+    len_list = []
+    for i, xcorr in enumerate(xcorr_list):
+        len_list.append(len(xcorr))
+        
+    npts_expected = mode(len_list)
+
+    for i, xcorr in enumerate(xcorr_list):
+        if len(xcorr) == npts_expected:
+            pass
+        else:
+            xcorr_new = xcorr.copy()
+            add_pts = npts_expected - len(xcorr)
+            for add in range(add_pts):
+                xcorr_new = np.append(xcorr_new, 0)
+                
+            xcorr_list[i] = xcorr_new
+            
+    return xcorr_list
    
 def multi_correlate(pair_list,low,high=None,time_method='points'):
+    """
+    Parameters
+    ----------
+    pair_list : list of length-2 lists of strings
+        List of lists, each sub-list being a pair of strings giving the file path
+        to the files to be cross-correlation
+    low : int or float
+        Low end of bandpass filter. Suggested 0.05Hz = 20 second period.
+    high : int or float, optional
+        High end of bandpass filter. Defaults to Nyquist frequency of the files.
+    time_method : string, optional
+        'points' or 'seconds': Gives the list of times as points or seconds.
+
+    Returns
+    -------
+    xcorr_list_fixed : list of numpy.ndarray
+        List containing the cross-correlation functions for each window given
+        as a numpy array.
+    xcorr_times : numpy.ndarray
+        Times for the cross-correlation function, given as a numpy array.
+    """
     xcorr_list = []
     
     for pair in pair_list:
         xcorr, xcorr_times, meta = cross_correlate_ambient_noise(pair,low,high,time_method)
+        xcorr_list.append(xcorr)
         
+    xcorr_list_fixed = check_correlation_length(xcorr_list)
     
-    
+    return xcorr_list_fixed, xcorr_times
 
-        
-    
-    
-    
-    
-    
-""" 
-def create_correlation_trace():
-    # Creates a trace with the ambient times and the completed function
-    pass
-"""
+def xcorr_stack(xcorr_list):
+    xcorr_stack = sum(xcorr_list) / len(xcorr_list)
+    return xcorr_stack
+
