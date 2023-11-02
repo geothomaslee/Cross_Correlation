0cca27c0eca4e8afa7ac272908e39a565a05a7d7
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-02 13:56:47 -0600

    Update Correlation_Main.py

--- Correlation_Main.py
+++ Correlation_Main.py
@@ -10,7 +10,7 @@ from correlation import cross_correlate_ambient_noise as corr
 import matplotlib.pyplot as plt
 from obspy.signal.filter import bandpass
 
-station1_files, station2_files = prep.find_station_files('ANMO', 'RSSD', '~/Documents/Correlation_Testing_Data')
+station1_files, station2_files = prep.find_station_files('ANMO', 'TUC', './Downloaded_Traces')
 
 df1, df2 = prep.get_info_from_file_name(station1_files, station2_files, name_structure=None)
 
